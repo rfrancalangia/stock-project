63fc837754a9ea858ca4a6ecbb458e6c79c3c427
Robert Francalangia <rcfran17@g.holycross.edu>
2021-09-29 11:17:57 -0400

    Before API call websetup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var exphbs  = require('express-handlebars');
+const exphbs  = require('express-handlebars');
+const axios = require('axios');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +13,18 @@ app.set('view engine', 'handlebars');
 
 //Set handlebar routes
 app.get('/', function (req, res) {
+    axios.get('')
     res.render('home', {
       stuff: "This is stuff"
     });
 });
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    axios.get('')
+    res.render('about')
+});
+
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
